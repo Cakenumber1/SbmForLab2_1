8c8672b83022264d31798cd06d90fec5c485b01b
Cakenumber1 <52410533+Cakenumber1@users.noreply.github.com>
2020-03-03 03:11:39 +0300

    Add files via upload

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+               while (false)
+               {
+               int i =0;
+               i++;
+               } 
+        }
+    }
+}
+// == != =>
